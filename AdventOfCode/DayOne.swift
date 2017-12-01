c35011a4a964abfad9e766ce907a067c21edf1b3
Paul Tiarks <paultiarks@gmail.com>
2017-12-01 10:15:14 -0600

    Complete first half of day one

---
+++ AdventOfCode/DayOne.swift
@@ -0,0 +1,41 @@
+//
+//  DayOne.swift
+//  AdventOfCode
+//
+//  Created by Paul Tiarks on 12/1/17.
+//  Copyright © 2017 Paul Tiarks. All rights reserved.
+//
+
+import Foundation
+
+struct DayOneCaptcha {
+    // Return the sum of number characters in the string that match the next character
+    static func solve(captcha: String) -> Int {
+        let characters = Array(captcha)
+//        guard let last = characters.last else { return 0 }
+        guard let first = characters.first else { return 0 }
+
+        var counter = 0
+        var sum = 0
+        for character in characters {
+            var next = character
+            if (counter + 1) == characters.count {
+                next = first
+            } else {
+                next = characters[counter + 1]
+            }
+
+            if let firstInt = Int(character.description),
+                let secondInt = Int(next.description) {
+                if firstInt == secondInt {
+                    sum += firstInt
+                }
+            }
+
+
+            counter += 1
+        }
+
+        return sum
+    }
+}
